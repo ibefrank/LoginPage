cb3b3ab30a2ae804d786db87aff992f7a943455e
Arweya Frank <158575678+ibefrank@users.noreply.github.com>
2025-07-09 17:20:57 +0300

    Add project files.

---
+++ LoginPage/Converters.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => !(bool)value;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => !(bool)value;
+}
+
+public class EyeIconConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => (bool)value ? "eye_off.png" : "eye.png";
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
